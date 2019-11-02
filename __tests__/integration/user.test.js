7698fa8e2a52d7c41881da70cfdff4b6a5097469
Andre Rego <andremregodev@gmail.com>
2019-11-01 23:52:55 -0300

    Utilizando o faker e factory-girl para gerar dados aleatorios

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -2,7 +2,7 @@ import request from 'supertest';
 import bcrypt from 'bcryptjs';
 import app from '../../src/app';
 
-import User from '../../src/app/models/User';
+import factory from '../factories';
 import truncate from '../util/trucate';
 
 describe('User', () => {
@@ -11,9 +11,7 @@ describe('User', () => {
   });
 
   it('should encrypt user password when new user created', async () => {
-    const user = await User.create({
-      name: 'Andre Marcelo',
-      email: 'andre@gmail.com',
+    const user = await factory.create('User', {
       password: '123456',
     });
     // Comparar senha inserida com a senha hash gerada no banco
@@ -24,33 +22,25 @@ describe('User', () => {
   });
 
   it('should be able to register', async () => {
+    const user = await factory.attrs('User');
+
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Andre Marcelo',
-        email: 'andre@gmail.com',
-        password: '123456',
-      });
+      .send(user);
 
     expect(response.body).toHaveProperty('id');
   });
 
   it('should not be able to register with duplicated email', async () => {
+    const user = await factory.attrs('User');
+
     await request(app)
       .post('/users')
-      .send({
-        name: 'Andre Marcelo',
-        email: 'andre@gmail.com',
-        password: '123456',
-      });
+      .send(user);
 
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Andre Marcelo',
-        email: 'andre@gmail.com',
-        password: '123456',
-      });
+      .send(user);
 
     expect(response.status).toBe(400);
   });
